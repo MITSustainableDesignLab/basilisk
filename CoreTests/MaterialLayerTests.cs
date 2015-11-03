bcf73865c036dde980ce193a547af29ef5a0a9d0
rosecodym <rosecodym@gmail.com>
2015-11-03 09:06:03 -0500

    add layer json roundtrip tests
    Timur was reporting problems with deserializing MaterialLayers so I added some tests to verify that things were working.

---
+++ CoreTests/MaterialLayerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Basilisk.Core;
+
+namespace Basilisk.Tests.Core
+{
+    [TestClass]
+    public class MaterialLayerTests
+    {
+        [TestMethod]
+        public void JsonRoundtrip_Glazing_IsEqual()
+        {
+            var layer = Dummies.GlazingLayer;
+            var roundtripped = JsonSerialization.Roundtrip(layer);
+            Assert.AreEqual(layer, roundtripped);
+        }
+
+        [TestMethod]
+        public void JsonRoundtrip_Gas_IsEqual()
+        {
+            var layer = Dummies.GasLayer;
+            var roundtripped = JsonSerialization.Roundtrip(layer);
+            Assert.AreEqual(layer, roundtripped);
+        }
+
+        [TestMethod]
+        public void JsonRoundtrip_Opaque_IsEqual()
+        {
+            var layer = Dummies.OpaqueLayer;
+            var roundtripped = JsonSerialization.Roundtrip(layer);
+            Assert.AreEqual(layer, roundtripped);
+        }
+    }
+}
