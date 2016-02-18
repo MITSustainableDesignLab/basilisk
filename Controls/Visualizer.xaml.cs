571fa1c414b564c122206504d38d3f7654e8d076
rosecodym <rosecodym@gmail.com>
2016-02-18 16:26:04 -0500

    add visualizer control and a few stubs

---
+++ Controls/Visualizer.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+using Basilisk.Controls.InterfaceModels;
+
+namespace Basilisk.Controls
+{
+    /// <summary>
+    /// Interaction logic for Visualizer.xaml
+    /// </summary>
+    public partial class Visualizer : UserControl
+    {
+        public Visualizer()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsAnythingVisualized
+        {
+            get { return (bool)GetValue(IsAnythingVisualizedProperty); }
+            set { SetValue(IsAnythingVisualizedProperty, value); }
+        }
+
+        public static DependencyProperty IsAnythingVisualizedProperty =
+            DependencyProperty.Register(
+                nameof(IsAnythingVisualized),
+                typeof(bool),
+                typeof(Visualizer));
+
+        public object VisualizedObject
+        {
+            get { return GetValue(VisualizedObjectProperty); }
+            set { SetValue(VisualizedObjectProperty, value); }
+        }
+
+        public static DependencyProperty VisualizedObjectProperty =
+            DependencyProperty.Register(
+                nameof(VisualizedObject),
+                typeof(object),
+                typeof(Visualizer),
+                new FrameworkPropertyMetadata((s, e) => ((Visualizer)s).DrawNewObject(e.NewValue)));
+
+        private void DrawNewObject(object o)
+        {
+            IsAnythingVisualized = o != null && Draw((dynamic)o);
+        }
+
+        private bool Draw(object o) => false;
+
+        private bool Draw(OpaqueMaterial c)
+        {
+            canvas.Background = Brushes.Red;
+            return true;
+        }
+
+        private bool Draw(GlazingMaterial c)
+        {
+            canvas.Background = Brushes.Blue;
+            return true;
+        }
+
+        private bool Draw(OpaqueConstruction c)
+        {
+            canvas.Background = Brushes.Green;
+            return true;
+        }
+    }
+}
