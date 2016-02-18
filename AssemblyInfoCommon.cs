f50308e5d4ce63f28d7c5bfbb2990a09aa456eb8
rosecodym <rosecodym@gmail.com>
2016-02-18 17:55:16 -0500

    break common assembly props into single file
    This allows us to start versioning this without having to edit five files each time.

---
+++ AssemblyInfoCommon.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Massachusetts Institute of Technology")]
+[assembly: AssemblyCopyright("Copyright © Massachusetts Institute of Technology 2015-2016")]
+[assembly: AssemblyProduct("Basilisk")]
+
+[assembly: AssemblyVersion("1.0.*")]
--- Controls/Properties/AssemblyInfo.cs
+++ Controls/Properties/AssemblyInfo.cs
@@ -7,16 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Controls")]
+[assembly: AssemblyTitle("Basilisk.Controls")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Controls")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: InternalsVisibleTo("Basilisk.ControlTests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -41,17 +36,3 @@ using System.Windows;
                                       //(used if a resource is not found in the page, 
                                       // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
