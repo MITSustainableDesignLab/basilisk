558953dfb36655a6c0c5651cfc55c9c20d74038e
rosecodym <rosecodym@gmail.com>
2019-08-10 17:36:25 -0400

    update build system

---
+++ pack-basilisk.ps1
@@ -0,0 +1,26 @@
+param (
+    [string]$Version,
+    [string]$TargetsDir = 'targets'
+)
+
+if (!$Version) {
+    $tags = git tag --points-at HEAD
+    $tags = $tags -split " "
+    foreach ($tag in $tags) {
+        if ($tag -match "^v(.+)") {
+            $Version = $Matches.1
+            write-host "Using version=$Version from Git tag $tag"
+            break
+        }
+    }
+}
+else {
+    write-host "Using version=$Version because it was explicitly passed as an argument"
+}
+
+if (!$Version) {
+    write-host "No version specified on the command line and no tags matching pattern ^v.+ on HEAD!"
+    exit 1
+}
+
+dotnet pack --no-build -p:PackageVersion=$Version -p:FileVersion=$Version -o $TargetsDir -c Release
