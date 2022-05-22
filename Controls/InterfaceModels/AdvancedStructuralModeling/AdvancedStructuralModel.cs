92b6b39fa0c633411b75c0c84f52119d6df3c432
rosecodym <rosecodym@gmail.com>
2022-05-22 12:08:43 -0400

    create loading shell

--- Controls/InterfaceModels/AdvancedStructuralModeling/AdvancedStructuralModel.cs
+++ Controls/InterfaceModels/AdvancedStructuralModeling/AdvancedStructuralModel.cs
@@ -1,4 +1,6 @@
 ﻿using Basilisk.Controls.Attributes;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Basilisk.Controls.InterfaceModels.AdvancedStructuralModeling;
 
@@ -8,4 +10,8 @@ public class AdvancedStructuralModel
     public ColumnWallSpacingSettings ColumnWallSpacing { get; set; }
 
     public ConstructionSystemSettings ConstructionSystems { get; set; }
+
+    public LoadingSettings LoadingSettings { get; set; } = new LoadingSettings();
+
+    public IEnumerable<LoadingSettings> LoadingSettingsList => Enumerable.Repeat(LoadingSettings, 1);
 }
---
+++ Controls/InterfaceModels/AdvancedStructuralModeling/LoadingSettings.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace Basilisk.Controls.InterfaceModels.AdvancedStructuralModeling;
+
+public class LoadingSettings : INotifyPropertyChanged
+{
+    private bool isLoadingValueEditable;
+    private double loadingValue;
+
+    public bool IsLoadingValueEditable
+    {
+        get => isLoadingValueEditable;
+        set
+        {
+            if (isLoadingValueEditable != value)
+            {
+                isLoadingValueEditable = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsLoadingValueEditable)));
+            }
+        }
+    }
+
+    public double LoadingValue
+    {
+        get => loadingValue;
+        set
+        {
+            if (loadingValue != value)
+            {
+                loadingValue = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LoadingValue)));
+            }   
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
