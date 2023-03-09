cedb8112dceef817e5eb7dc488d6ecb6dde5c1f9
rosecodym <rosecodym@gmail.com>
2023-03-09 12:58:46 -0500

    add foundation soil

---
+++ Core/AdvancedStructuralModeling/FoundationSoilPresetMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Basilisk.Core.AdvancedStructuralModeling;
+
+public static class FoundationSoilPresetMap
+{
+    private static readonly Dictionary<FoundationSoilPreset, double> map = new()
+    {
+        [FoundationSoilPreset.Clay] = 72,
+        [FoundationSoilPreset.Gravel] = 97,
+        [FoundationSoilPreset.Sand] = 144
+    };
+
+    public static double? TryGetValue(FoundationSoilPreset preset)
+    {
+        return map.TryGetValue(preset, out var value)
+            ? value
+            : null;
+    }
+}
